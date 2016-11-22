8f6aa8d177187912fe17616a84cccff3d13aacf4
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-11-22 16:38:03 -0200

    teste geral

--- Teste.sv
+++ Teste.sv
@@ -1,26 +1,26 @@
-module Teste cont2;
-    logic clock, resst;
-    wire[3:0] sa;
+module TesteGeral;
+    logic clock, reset;
+    wire[3:0] sa1, sa2;
 
     initial begin
-
         clock = 0;
         reset = 1;
-        
+
         #10;
         reset = 0;
 
-        $display(sa);
+        $display("C1: %d | C2: %d", sa1, sa2);
 
-        repeat(100) begin
+        repeat(50) begin
 
             if(clock)
-                $display(sa);
+                $display("C1: %d | C2: %d", sa1, sa2);
             
             clock = ~clock;
             #10;
         end
     end
 
-    Cont2 c1(.clock(clock), .sa(sa), .reset(reset));
+    Cont1 c1(.clock(clock), .sa(sa1), .reset(reset));
+    Cont2 c2(.clock(clock), .sa(sa2), .reset(reset));
 endmodule
