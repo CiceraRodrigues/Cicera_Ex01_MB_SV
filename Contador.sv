0b5934defaa845532f1765adc07b48ccc74ae098
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-11-22 16:36:48 -0200

    contador segundo caso

--- Contador.sv
+++ Contador.sv
@@ -1,20 +1,32 @@
-module Cont1(input logic reset,clock output logic[3:0] sa);
-    logic direction;
+module Cont2(input logic reset,clock output logic[3:0] s);
+    logic direction, g1, g2;
 
     always @(posedge clock or posedge reset)
 
         if(reset) begin
             sa = 4'd0;
             direction = 1'b0;
+            g1 = 1'b1;
+            g2 = 1'b0;
         end
 
         else begin
-            if(direction)
-                sa = sa - 4'd1;
-            else
-                sa = sa + 4'd1;
 
-            if(sa==4'd15 || sa==4'd0)
+            if(g1) begin
+                if(direction)
+                    sa = s - 4'd1;
+                else
+                    sa = sa + 4'd1;
+
+                g2 = 1'b1;
+            end
+            else
+                g1 = 1'b1;
+
+            if((sa==4'd15 || s==4'd0) && g2) begin
                 direction = ~direction;
+                g1 = 1'b0;
+                g2 = 1'b0;
+            end
         end
 endmodule
