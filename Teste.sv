8eefa1222d785223c10d64d3046058eaa5a98635
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-11-22 16:33:01 -0200

    teste do contador primeiro caso

---
+++ Teste.sv
@@ -0,0 +1,26 @@
+module Teste cont1;
+    logic clock, reset;
+    wire[3:0] sa;
+
+    initial begin
+
+        clock = 0;
+        reset = 1;
+
+        #10;
+        reset = 0;
+
+        $display(sa);
+
+        repeat(50) begin
+
+            if(clock)
+                $display(sa);
+            
+            clock = ~clock;
+            #10;
+        end
+    end
+
+    Cont1 c1(.clock(clock), .sa(sa), .reset(reset));
+endmodule
